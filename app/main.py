d1f55664e4faf01c81968ab2fd8bb1cd73a1a911
Ilya Makarov <ilya@cinetronix.co.uk>
2025-01-31 04:23:29 +0000

    Add application source files and Python related configs

---
+++ app/main.py
@@ -0,0 +1,21 @@
+import logging
+
+import yaml
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+
+with open('logging.yaml', 'rt') as f:
+    logging_config = yaml.safe_load(f.read())
+
+logging.config.dictConfig(logging_config)
+
+
+app = FastAPI(
+    title="FastAPI-app-example",
+    description="A simple FastAPI application that returns 'Hello, World!' at the root endpoint (/).",
+)
+
+
+@app.get("/", response_class=PlainTextResponse)
+async def read_root():
+    return "Hello, World!"
